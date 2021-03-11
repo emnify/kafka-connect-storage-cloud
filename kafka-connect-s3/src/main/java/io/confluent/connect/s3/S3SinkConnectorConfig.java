4dbfb83017790c3ee25fd58ffda21e2fe7ee8093
Jiri Syrovy <jiri.syrovy@emnify.com>
2021-03-11 10:52:28 +0100

    Allow pattern filename

--- kafka-connect-s3/src/main/java/io/confluent/connect/s3/S3SinkConnectorConfig.java
+++ kafka-connect-s3/src/main/java/io/confluent/connect/s3/S3SinkConnectorConfig.java
@@ -123,6 +123,7 @@ public class S3SinkConnectorConfig extends StorageSinkConnectorConfig {
   public static final String COMPRESSION_TYPE_CONFIG = "s3.compression.type";
   public static final String COMPRESSION_TYPE_DEFAULT = "none";
 
+
   public static final String COMPRESSION_LEVEL_CONFIG = "s3.compression.level";
   public static final int COMPRESSION_LEVEL_DEFAULT = Deflater.DEFAULT_COMPRESSION;
   private static final CompressionLevelValidator COMPRESSION_LEVEL_VALIDATOR =
@@ -153,6 +154,9 @@ public class S3SinkConnectorConfig extends StorageSinkConnectorConfig {
 
   public static final String BEHAVIOR_ON_NULL_VALUES_CONFIG = "behavior.on.null.values";
   public static final String BEHAVIOR_ON_NULL_VALUES_DEFAULT = BehaviorOnNullValues.FAIL.toString();
+
+  public static final String S3_FILENAME_PATTERN_CONFIG = "s3.path.filename.format";
+  public static final String S3_FILENAME_PATTERN_DEFAULT = "";
 
   /**
    * Maximum back-off time when retrying failed requests.
@@ -649,6 +653,18 @@ public class S3SinkConnectorConfig extends StorageSinkConnectorConfig {
           Width.NONE,
           "Headers format class",
           HEADERS_FORMAT_CLASS_RECOMMENDER
+      );
+
+      configDef.define(
+              S3_FILENAME_PATTERN_CONFIG,
+              Type.STRING,
+              S3_FILENAME_PATTERN_DEFAULT,
+              Importance.LOW,
+              "Specifies filename pattern (use %t for topic, %p for partition and %o for offset",
+              group,
+              ++orderInGroup,
+              Width.SHORT,
+              "Filename pattern"
       );
 
       configDef.define(
